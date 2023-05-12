d8fd8ca872f05600cb9d8d5b5fafab416b15fe32
EdselCasey <edselcasey@yahoo.com>
2023-05-12 09:05:24 -0400

    finished all excercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(count) {
+    let firstNum = 0;
+    let secondNum = 1;
+    if (count < 0) {
+        return "OOPS";
+    }
+    if (count === 0) {
+        return 0;
+    }
+    for (i = 1; i < count; i++){
+        let sum = secondNum;
+        secondNum = firstNum + secondNum;
+        firstNum = sum;
+    }
+    return secondNum;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const oldest = people.reduce((oldestPerson, currentPerson) => {
+        const oldestAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge < currentAge ? currentPerson : oldestPerson;
+      });
+      return oldest;
 };
+
+const getAge = function(birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  }
 
 // Do not edit below this line
 module.exports = findTheOldest;
